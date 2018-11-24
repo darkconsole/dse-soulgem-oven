07d5db2304eb7933647fcae4943a5d1719a74852
darkconsole <darkcee.legit@gmail.com>
2018-11-23 21:28:56 -0600

    script to make release zips

---
+++ release.bat
@@ -0,0 +1,6 @@
+@echo off
+
+echo DID YOU INCREMEMENT THE VERSION NUMBER IN THE MAIN SCRIPT ASSHOLE?
+
+c:\local\unxutils\usr\local\wbin\zip dse-soulgem-oven-v%~1.zip -q -r *.esp configs interface meshes scripts textures
+
