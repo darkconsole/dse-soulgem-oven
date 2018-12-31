b4d16aab043d28f4c2ff8ad9a0dcf9735a0c17c9
darkconsole <darkcee.legit@gmail.com>
2018-12-31 17:44:22 -0600

    include bodyslide files in releases

--- release.bat
+++ release.bat
@@ -2,5 +2,5 @@
 
 echo DID YOU INCREMEMENT THE VERSION NUMBER IN THE MAIN SCRIPT ASSHOLE?
 
-c:\local\unxutils\usr\local\wbin\zip dse-soulgem-oven-v%~1.zip -q -r *.esp configs interface meshes scripts textures
+c:\local\unxutils\usr\local\wbin\zip dse-soulgem-oven-v%~1.zip -q -r *.esp configs interface meshes scripts textures CalienteTools
 
