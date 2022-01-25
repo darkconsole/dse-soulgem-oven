f31f7da1d9cbe96b5be845acd06e17f8399c705c
darkconsole <darkcee.legit@gmail.com>
2022-01-25 13:41:09 -0600

    full recompile

--- recompile.bat
+++ recompile.bat
@@ -3,7 +3,7 @@
 SET Compiler=C:\Games\MOSSE\mo-script-compile.bat
 SET Output=C:\Games\MOSSE\mods\dse-soulgem-oven
 
-FOR /R "%Output%\Scripts" %%F IN (*) DO (
-	call %Compiler% %%~nF %Output%
-	TIMEOUT /t 10
+FOR /R "%Output%\Scripts\Source" %%F IN (*.psc) DO (
+	CALL %Compiler% %%~nF %Output%
+	SLEEP 6
 )
